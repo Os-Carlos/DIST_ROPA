995a8d0493c8f452f22b4577fbbf38abf92a5775
carlos osorio <cordonezo4@miumg.edu.gt>
2023-10-16 19:23:20 -0600

    database config

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "db_dist_ropa",
+    HOST: "dpg-ckgajgi12bvs73e54uqg-a.oregon-postgres.render.com",
+    USER: "dw2023",
+    PASSWORD: "Gi6GaiaMdT6Q5LbtCEuwHJR4yU92qHsU",
+    DB: "bd_dist_ropa",
     PORT: "5432",
     dialect: "postgres",
     pool: {
