13798b2ae0b104df9eff0ac4d007b1cee3a95475
carlos osorio <cordonezo4@miumg.edu.gt>
2023-10-30 12:46:08 -0600

    update dashboard

--- frontend/src/pages/Inicio/index.js
+++ frontend/src/pages/Inicio/index.js
@@ -4,11 +4,12 @@ import { Image } from 'antd'
 export default function Inicio() {
     return (
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: 1140 }}>
-            <h2> No está al 100</h2>
 
             <Image
-                width={400}
-                src='https://zgcgkizkfhatmwkijhpw.supabase.co/storage/v1/object/public/docs_empresa/kafnlkfls.jpg'></Image>
+                width={850}
+                preview={false}
+                style={{ marginTop: 20 }}
+                src='https://zgcgkizkfhatmwkijhpw.supabase.co/storage/v1/object/public/docs_empresa/sales-performance-dashboard.png'></Image>
         </div>
     )
 }
